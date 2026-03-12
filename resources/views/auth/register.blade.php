96949aef3b7b2fc9ff90cb46376ecb81d380a4c0
OscarRodriguez90 <461853.joan23@fje.edu>
2026-03-12 17:27:50 +0100

    logo en login y register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,10 +12,7 @@
 
     <!-- Brand -->
     <a href="#" class="brand">
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z"/>
-            <circle cx="12" cy="10" r="3"/>
-        </svg>
+        <img src="{{ asset('img/admin/logo.png') }}" alt="GeoTurismo" class="brand-logo">
         GeoTurismo
     </a>
 
